a659ad3f83a77381e09cbb0ae93731869632d02e
Muhammad Sohaib <93024641+MuhammadSohaib-pro@users.noreply.github.com>
2026-02-09 14:43:40 +0500

    feat: header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const { homepage, title } = header;
 
   return (
-    <header className="px-4 container h-24 md:h-32 w-full mx-auto my-0 flex items-center justify-between">
+    <header className="px-4 container h-24 md:h-32 w-full mx-auto my-0 z-50 bg-accent sticky top-0 md:static md:bg-transparent  flex items-center justify-between">
       <h3 className="text-2xl font-bold">
         {homepage ? (
           <a href={homepage} className="link text-primary relative">
